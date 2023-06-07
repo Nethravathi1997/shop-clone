bfce7b36c585e0ddecb0271267e10d1d86a054b3
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2023-06-07 18:03:09 +0530

    Update checkout.js

--- components/checkout.js
+++ components/checkout.js
@@ -1,7 +1,3 @@
-let date = document.getElementById("date");
-date.innerHTML= "Get your Order By" + " <br><br>"+  new Date(0,0,0,0,0,0,Date.now() + 5 * 24 * 60 * 60 * 1000);
-
-
 
 
   //Registration form validation
