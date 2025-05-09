adc505fed091cb0d02ec5044f189f7286580242b
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2025-05-09 14:38:59 +0530

    Update common.js

--- components/common.js
+++ components/common.js
@@ -5,19 +5,15 @@ side.innerHTML = sidebar();
 import navbar from "./navbar.js";
 let nav = document.getElementById("navbar");
 nav.innerHTML = navbar();
-
 import footer from "./footer.js";
 let foot = document.getElementById("footer");
 foot.innerHTML = footer();
-
 function openNav() {
   document.getElementById("mySidebar").style.display = "block";
 }
-
 function closeNav() {
   document.getElementById("mySidebar").style.display = "none";
 }
-
 document.getElementById("sd").addEventListener("click", () => {
   openNav();
 });
